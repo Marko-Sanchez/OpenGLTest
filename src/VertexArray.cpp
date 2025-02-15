ae835587b4aa94701a775f99e56b2ab2130e86ab
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-02-14 21:19:02 -0800

    Added glVertexAttribIPointer() for GL_INT and GL_UNSIGNED_INT: The general type of attribute used in the vertex shader must match the general type provided by the attribute array. This is governed by which glVertexAttribPointer function you use. For floating-point attributes, you must use glVertexAttribPointer. For integer (both signed and unsigned), you must use glVertexAttribIPointer. And for double-precision attributes, where available, you must use glVertexAttribLPointer.

--- src/VertexArray.cpp
+++ src/VertexArray.cpp
@@ -1,5 +1,6 @@
 #include "VertexArray.h"
 #include "VertexBuffer.h"
+#include <iostream>
 
 VertexArray::VertexArray()
 {
@@ -23,17 +24,26 @@ void VertexArray::UnBind() const
 
 void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& vbl)
 {
-    Bind();
     vb.Bind();
+    Bind();
 
     unsigned int offset{0};
     const auto& elements = vbl.GetElements();
 
     for(unsigned int i{0}; i < elements.size(); ++i)
     {
-        glVertexAttribPointer(i, elements[i].count, elements[i].type, elements[i].normalized, vbl.GetStride(), reinterpret_cast<void*>(offset));
+        if ( elements[i].type == GL_INT || elements[i].type == GL_UNSIGNED_INT)
+        {
+            glVertexAttribIPointer(i, elements[i].count, elements[i].type, vbl.GetStride(), reinterpret_cast<void*>(offset));
+        }
+        else
+        {
+            glVertexAttribPointer(i, elements[i].count, elements[i].type, elements[i].normalized, vbl.GetStride(), reinterpret_cast<void*>(offset));
+        }
+
         glEnableVertexAttribArray(i);
 
+        std::cout << i << ": " << "Offset: " << offset << ", Count: " << elements[i].count << " , Size: " << VertexBufferElement::GetSizeOfType(elements[i].type) << std::endl;
         offset += elements[i].count * VertexBufferElement::GetSizeOfType(elements[i].type);
     }
 }
