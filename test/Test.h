86b440e503f17017e888929a55ba9f153a849615
Marko-Sanchez <sanchezmarco0909@gmail.com>
2025-05-24 23:07:51 -0700

    Removed unused virtual function OnUpdate(), updated comments, RegisterTest() now takes a ref to string instead of a copy to one

--- test/Test.h
+++ test/Test.h
@@ -14,21 +14,19 @@ namespace tests
         Test(){}
         virtual ~Test(){}
 
-        virtual void OnUpdate(float deltaTime){}
         virtual void OnRender(){}
         virtual void OnImGuiRender(){}
     };
 
     // When we first run the app, TestMenu will present a series of buttons representing
-    // different test. When a test is selected via OnImGuiRender returns a lambda and constructs
+    // different test. When a test is selected via OnImGuiRender we return a lambda and construct
     // test on demand, instead of loading all the test in the beginning of the application.
     class TestMenu: public Test
     {
         private:
         // Pointer to window context, to be shared with tests.
         std::shared_ptr<void> g_window;
-        // Reference to a ptr outside this class, current drawn test is updated via this variable
-        // in OnImGuiRender.
+        // Reference to a ptr outside this class, current selected test is updated via this variable.
         std::shared_ptr<Test>& m_currentTest;
         std::vector<std::pair<std::string, std::function<std::shared_ptr<Test>()>>> m_tests;
 
@@ -39,7 +37,7 @@ namespace tests
 
         // creates a vector of test, using a lambda to build test on demand.
         template<typename T>
-        void RegisterTest(const std::string name)
+        void RegisterTest(const std::string& name)
         {
             std::cout << "Registering test: " << name << std::endl;
             m_tests.push_back(std::make_pair(name, [](){return std::make_shared<T>(); }));
